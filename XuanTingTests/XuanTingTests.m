dc501e53d55a9ab2bd325f0825f5d1de8cd0605c
feng.ma <feng.ma@patpat.com>
2018-01-22 16:26:40 +0800

    Initial Commit

---
+++ XuanTingTests/XuanTingTests.m
@@ -0,0 +1,39 @@
+//
+//  XuanTingTests.m
+//  XuanTingTests
+//
+//  Created by patpat on 2018/1/22.
+//  Copyright © 2018年 test. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface XuanTingTests : XCTestCase
+
+@end
+
+@implementation XuanTingTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
